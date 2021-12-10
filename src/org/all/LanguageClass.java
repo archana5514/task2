f07e7f61697d163e95b075c5c789e0369d91923b
Archana5514 <archanasmk1997@gmail.com>
2021-12-10 15:46:47 +0530

    Hi

---
+++ src/org/all/LanguageClass.java
@@ -0,0 +1,20 @@
+package org.all;
+import org.english.English;
+public class LanguageClass extends English {
+	private void allLanguage() {
+		System.out.println("All Language");
+		// TODO Auto-generated method stub
+
+	}
+	public static void main(String[] args) {
+		LanguageClass l = new LanguageClass();
+		l.allLanguage();
+		l.englishLanguage();
+		l.tamilLanguage();
+		l.teluguLanguage();
+		
+		
+		
+	}
+
+}
